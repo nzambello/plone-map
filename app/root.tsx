3c78d03a4042bb03a3d8d2bdac9fa81bc187effd
nzambello <nzambello@protonmail.com>
2022-11-28 12:29:13 +0100

    Add plausible, seo meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,25 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { MetaFunction } from '@remix-run/cloudflare'
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'The Plone Map',
+  description: 'This is a map of Plone Foundation members',
+  viewport: 'width=device-width,initial-scale=1',
+  language: 'en',
+  'og:title': 'The Plone Map',
+  'og:description': 'This is a map of Plone Foundation members',
+  'og:site_name': 'The Plone Map',
+  'og:type': 'website',
+  'og:locale': 'en',
+  'twitter:card': 'summary',
+  'twitter:title': 'The Plone Map',
+  'twitter:description': 'This is a map of Plone Foundation members',
+  'twitter:site': '@plone',
+  'twitter:creator': '@plone',
+  'og:image': 'https://plone.org/logo.png',
+  'twitter:image': 'https://plone.org/logo.png'
+})
 
 export default function App() {
   return (
@@ -20,6 +27,7 @@ export default function App() {
       <head>
         <Meta />
         <Links />
+        <script defer data-domain="plone-map.pages.dev" src="https://plausible.io/js/script.js"></script>
       </head>
       <body>
         <Outlet />
@@ -28,5 +36,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
